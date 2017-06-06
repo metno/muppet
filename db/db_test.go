50c429bee09ed8ff40d879322d9e1918256c898c
Kim Tore Jensen <kimtj@met.no>
2017-06-06 09:38:12 +0200

    Expand test for db.Database, checking job equality.

--- db/db_test.go
+++ db/db_test.go
@@ -6,23 +6,34 @@ import (
 
 	"github.com/metno/muppet/db"
 	"github.com/metno/muppet/job"
+	"github.com/stretchr/testify/assert"
 )
 
-// Test that the same amount of input jobs are output back on the job queue.
+// Test that input jobs are echoed back on the job queue after persisting.
 func TestEcho(t *testing.T) {
-	max := db.MAX_QUEUE
-	db := db.New()
-	go db.Run()
-	for i := 0; i < max; i++ {
-		j := job.New()
-		db.In <- j
+
+	// Generate MAX_QUEUE jobs.
+	jobs := make([]job.Job, db.MAX_QUEUE)
+	for i := 0; i < db.MAX_QUEUE; i++ {
+		jobs[i] = job.New()
 	}
 
+	// Submit all jobs asynchronously to the database thread.
+	dbThread := db.New()
+	go dbThread.Run()
+	go func() {
+		for i := 0; i < db.MAX_QUEUE; i++ {
+			dbThread.In <- jobs[i]
+		}
+	}()
+
 	// Receive all messages from the DB channel, but wait at most 1 second.
+	// Assert equality on all job objects.
 	timeout := time.After(1 * time.Second)
-	for i := 0; i < max; i++ {
+	for i := 0; i < db.MAX_QUEUE; i++ {
 		select {
-		case <-db.Out:
+		case j := <-dbThread.Out:
+			assert.Equal(t, jobs[i], j)
 		case <-timeout:
 			t.Logf("Not enough data available on output queue.")
 			t.Fail()
